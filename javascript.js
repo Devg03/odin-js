7f575119ed217955ffbd3ef714d716c3e1b5e153
WildWolf <wildwolf@wildwolf-pc.mynetworksettings.com>
2023-08-30 14:11:28 -0400

    Fundamentals Part 1

--- javascript.js
+++ javascript.js
@@ -1,1 +1,30 @@
+// Hello World
 console.log("Hello World");
+
+// Fundamentals Part 1
+console.log(23 + 97) // 1
+
+// 2
+let seq = 0
+for (let i = 1; i < 6; i++) {
+    seq += i;
+}
+console.log(seq);
+
+// 3
+console.log((4+6+9) / 77);
+
+// 4
+let a = 10
+console.log(a);
+console.log(9*a);
+
+let b = 7*a
+console.log(b);
+
+// 5
+const max = 57
+let actual = max - 13
+let percentage = actual / max
+
+console.log(percentage);
